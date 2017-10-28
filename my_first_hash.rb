4248f9ae990c5c1569b46b8150d5aabcc5140d12
Nolan Hughes <nhughes987@gmail.com>
2017-10-28 03:15:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {"name" => "Nolan", "best_friend" => "Jessie"}  
 end
 
 
@@ -9,7 +8,7 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information: 
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  shipping_manifest = {"whale bone corset" => 5, }
 
 end
 
